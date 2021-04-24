c773e277780e543fb7a5f3750824af95a758c9ba
Nitemoss <uhinemine@gmail.com>
2021-04-24 13:09:04 +0300

    jwt

--- cors/cors-server.go
+++ cors/cors-server.go
@@ -4,21 +4,83 @@ import (
 	"net/http"
 	"io"
 	"fmt"
-	// "encoding/json"
+	// "crypto/sha256"
+	// "strings"
+	"time"
+	"encoding/json"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
+var jwtKey = []byte("my_secret_key")
+
+
+type UserClaims struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+
+	jwt.StandardClaims
+}
+
+
+var users = map[string]string{
+	"root": "password#12345",
+	"user2": "password2",
+}
 
 
 func Handler(w http.ResponseWriter, req *http.Request) {
+	// db := map[string]string{ // checksum = sha256(username + " " + password)
+	//   "root": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855с",
+	// }
+
 	w.Header().Set("Access-Control-Allow-Origin", "*")
   w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH, OPTIONS")
   w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	w.WriteHeader(http.StatusOK)
+	// w.WriteHeader(http.StatusOK)
 
-	fmt.Printf("%+v\n", req)
-	fmt.Println(req.Method)
-	// fmt.Println(req.Body)
+	// fmt.Printf("%+v\n", req)
+	// fmt.Println(req.Method)
+
 	data, err := io.ReadAll(req.Body)
+
+	var raw map[string]interface{}
+    if err := json.Unmarshal(data, &raw); err != nil {
+        panic(err)
+    }
+
+	var method_name = raw["method_name"]
+	mySigningKey := []byte("AllYourBase")
+
+
+
+	if method_name == "auth" {
+		claims := UserClaims{
+			raw["username"],
+			raw["password"],
+
+			jwt.StandardClaims{
+				ExpiresAt: 15000,
+				Issuer:    "test",
+			},
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		ss, err := token.SignedString(mySigningKey)
+		fmt.Printf("%v %v", ss, err)
+
+	}
+	fmt.Println("\n\n")
+	// for name, values := range req.Header {
+	//     // Loop over all values for the name.
+	//     for _, value := range values {
+	//         fmt.Println(name, value)
+	//     }
+	// }
+
+	fmt.Println(req.Header.Get("Authorization"))
+
+
+
 	if req.Method == "POST" {
 
 		req.Body.Close()
@@ -43,8 +105,6 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Error: not allowed method")
 		return
 	}
-
-
 
 	io.WriteString(w, "You are on page1")
 }
--- cors/web/js/scripts.js
+++ cors/web/js/scripts.js
@@ -1,5 +1,18 @@
-async function request(url, data, method) {
+
+var auth_token = null
+
+async function request(url, data, method, username, password) {
 	document.getElementById("resp").innerHTML = "Loading...";
+	document.getElementById("auth_status").innerHTML = auth_token ? "Authorized" : "No access token";
+
+	if(auth_status){
+		document.getElementById("username").disabled = true
+		document.getElementById("password").disabled = true
+
+	}
+
+
+	console.log(document.getElementById("username").value, document.getElementById("password").value)
 	console.log(method)
 		try {
 			let res = await fetch(url, {
@@ -8,12 +21,18 @@ async function request(url, data, method) {
 				cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
 				credentials: 'same-origin', // include, *same-origin, omit
 				headers: {
-				  'Content-Type': 'application/json'
-				  // 'Content-Type': 'application/x-www-form-urlencoded',
+				  'Content-Type': 'application/json',
+					// 'Authorization': `Bearer username="${username}" Realm="${password}"`,
+					'Accept': 'application/json',
+				  'Content-Type': 'application/x-www-form-urlencoded',
 				},
 				redirect: 'follow', // manual, *follow, error
 				referrerPolicy: 'no-referrer', // no-referrer, *client
-				body: data // body data type must match "Content-Type" header
+				body: JSON.stringify({
+					"method_name": "auth",
+					"username": username,
+					"password": password
+				})// body data type must match "Content-Type" header
 			  });
 			let inf = await res;
 			console.log(inf);
@@ -28,7 +47,16 @@ async function request(url, data, method) {
 
 }
 
+
+
+
 function pop() {
 
-	request(document.getElementById("url").value, document.getElementById("body").value, document.getElementById("method").value);
+	request(
+		document.getElementById("url").value,
+		document.getElementById("body").value,
+		document.getElementById("method").value,
+		document.getElementById("username").value,
+		document.getElementById("password").value
+	);
 }
