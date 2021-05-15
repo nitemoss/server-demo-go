8a602460b18742bc26dedcbb019c8bcdd16ba544
Nitemoss <uhinemine@gmail.com>
2021-05-15 12:40:42 +0300

    msg input fix

--- websocket/websocketclient.go
+++ websocket/websocketclient.go
@@ -7,11 +7,13 @@ import (
 	"fmt"
 	"strings"
 	"github.com/gorilla/websocket"
+	"bufio"
+  "os"
 )
 
 func wait(c *websocket.Conn){
 	for {
-		
+
 		_, message, err := c.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
@@ -24,6 +26,10 @@ func wait(c *websocket.Conn){
 }
 
 func main() {
+	in := bufio.NewReader(os.Stdin)
+
+
+
 	c, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/socket", nil)
 	if err != nil {
 		log.Fatal("dial:", err)
@@ -36,9 +42,11 @@ func main() {
 	defer c.Close()
 	go wait(c)
 	for {
-		var msg string
+		// var msg string
 		fmt.Print("msg: ")
-		fmt.Scanf("%s", &msg)
+		// fmt.Scanln("%s", &msg)
+		msg, _ := in.ReadString('\n')
+
 
 		c.WriteMessage(websocket.TextMessage, []byte(name + "%%%%" + msg))
 
